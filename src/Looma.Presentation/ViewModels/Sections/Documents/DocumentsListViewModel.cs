213113d917d4893de91d79e36b6f18f338df2037
Timëo <t.soeur@student.helmo.be>
2026-06-07 14:42:11 +0200

    add headers

--- src/Looma.Presentation/ViewModels/Sections/Documents/DocumentsListViewModel.cs
+++ src/Looma.Presentation/ViewModels/Sections/Documents/DocumentsListViewModel.cs
@@ -1,3 +1,7 @@
+// Copyright (c) 2026 SOEUR Timëo. All rights reserved.
+// This file is part of Looma, licensed under the AGPL-3.0.
+// See LICENSE in the project root for full license text.
+
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
--- src/Looma.Presentation/ViewModels/Sections/Projects/ProjectsListViewModel.cs
+++ src/Looma.Presentation/ViewModels/Sections/Projects/ProjectsListViewModel.cs
@@ -1,3 +1,7 @@
+// Copyright (c) 2026 SOEUR Timëo. All rights reserved.
+// This file is part of Looma, licensed under the AGPL-3.0.
+// See LICENSE in the project root for full license text.
+
 using CommunityToolkit.Mvvm.ComponentModel;
 using Looma.Presentation.Navigation;
 using Looma.Presentation.ViewModels.Base;
