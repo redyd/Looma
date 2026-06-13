66d73de004dbd0ca38b0e582ae929922e847c522
Timëo <t.soeur@student.helmo.be>
2026-06-13 21:58:19 +0200

    enum display label

--- src/Looma.Domain/Core/StockAdjustmentMode.cs
+++ src/Looma.Domain/Core/StockAdjustmentMode.cs
@@ -2,11 +2,16 @@
 // This file is part of Looma, licensed under the AGPL-3.0.
 // See LICENSE in the project root for full license text.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Looma.Domain.Core;
 
 public enum StockAdjustmentMode
 {
+    [Display(Name = "Par pelote")]
     ByBall,
+    [Display(Name = "Par poids (g)")]
     ByWeight,
+    [Display(Name = "Par longueur (m)")]
     ByLength
 }
