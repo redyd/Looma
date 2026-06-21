ea26d0e88c29da91c564aac1cecf14ccddea0a03
Timëo <t.soeur@student.helmo.be>
2026-06-21 11:18:27 +0200

    fix colors

---
+++ src/Looma.Infrastructure/Entity/TrackedWool.cs
@@ -0,0 +1,15 @@
+namespace Looma.Infrastructure.Entity;
+
+public class TrackedWool
+{
+    public string Id { get; set; } = null!;
+    public DateTime Date { get; set; }
+    public double StockUsed { get; set; }
+
+
+    public int WoolId { get; set; }
+    public WoolEntity WoolEntity { get; set; } = null!;
+
+    public int ProjectId { get; set; }
+    public ProjectEntity ProjectEntity { get; set; } = null!;
+}
