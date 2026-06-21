5fccf9192a408a8b9c93d0bbdc34180520b458e2
Timëo <t.soeur@student.helmo.be>
2026-06-21 12:11:14 +0200

    fix headers

--- src/Looma.Infrastructure/Migrations/20260621094556_TrackedWool.cs
+++ src/Looma.Infrastructure/Migrations/20260621094556_TrackedWool.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) 2026 SOEUR Timëo. All rights reserved.
+// This file is part of Looma, licensed under the AGPL-3.0.
+// See LICENSE in the project root for full license text.
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
